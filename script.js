00340bc31c91bf0e67aa4fdeabb60509f9aa287f
guytepper <guytepper@gmail.com>
2016-01-24 20:42:56 +0200

    attached event listener, prototype is working

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 var view = (function view() {
-  var input = document.getElementById('input').value,
+  var input = document.getElementById('input'),
       output = document.getElementById('output'),
       html;
 
   function setOutput(input) {
-    html = converter.convert(input);
+    html = converter.convert(input.value);
     output.innerHTML = html;
+  }
+
+  function attachListeners() {
+    input.addEventListener('keyup', function() {
+      console.log('Key up.');
+      setOutput(input);
+    })
   }
 
   var viewAPI = {
     setOutput: setOutput,
+    attachListeners: attachListeners,
   };
-  
+
   return viewAPI;
 
 })();
@@ -32,4 +40,16 @@ var converter = (function converter(showdown) {
 
 })(showdown);
 
-var app = (function())
+var app = (function() {
+  function init() {
+    view.attachListeners();
+  }
+
+  var appAPI = {
+    init: init,
+  };
+
+  return appAPI;
+})();
+
+app.init();
