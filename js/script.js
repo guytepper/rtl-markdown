8b777334c145b8dcc4d2603dd5f89d20392b2340
guytepper <guytepper@gmail.com>
2016-01-26 13:31:17 +0200

    web font performance improvements
    implemented CSS Font Loading API (with fallback)

--- js/script.js
+++ js/script.js
@@ -34,6 +34,7 @@ var converter = (function converter(showdown) {
     var matches = [];
     return [
         {
+            type: 'lang',
             regex: /<%ltr%>([^]+?)<%endltr%>/gi,
             replace: function(s, match) {
                 matches.push(match); // pushes original content
@@ -65,7 +66,6 @@ var converter = (function converter(showdown) {
 
   var markdown = new showdown.Converter({ extensions: ['ltr'] });
 
-
   // Converts the input to Markdown using showdown.js
   function convert(input) {
     html = markdown.makeHtml(input);
@@ -82,10 +82,50 @@ var converter = (function converter(showdown) {
 
 var app = (function() {
 
-  // var openSans = new FontFace('Open Sans Hebrew', "u")
+  function loadFonts() {
+    // Fonts declrations
+    var openSans = new FontFace('Open Sans Hebrew',
+        'url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Regular.woff) format("woff")',
+        { weight: '400' });
+    var openSansBold = new FontFace('Open Sans Hebrew',
+        'url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Bold.woff) format("woff")',
+        { weight: '700' });
+
+    function loader(font) {
+      font.load().then(function() {
+                  console.log(font);
+                  document.fonts.add(font);
+                }).catch(function() {
+                  console.log("font failed to load");
+                });
+    }
+    loader(openSansBold); // for title
+    loader(openSans); // for body
+  }
+
+  // Adding @font-face to <head> if browser not supporting
+  // the CSS Font Loading API
+  function loadFontsFallback() {
+    var style = document.createElement('style');
+
+    // Regular @font-face [minified]
+    var openSans = "@font-face{font-family:'Open Sans Hebrew';font-style:normal;font-weight:400;src:url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Regular.woff2) format('woff2'),url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Regular.woff) format('woff'),url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Regular.ttf) format('truetype')}";
+    var openSansBold = "@font-face{font-family:'Open Sans Hebrew';font-style:normal;font-weight:700;src:url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Bold.woff2) format('woff2'),url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Bold.woff) format('woff'),url(https://fonts.gstatic.com/ea/opensanshebrew/v3/OpenSansHebrew-Bold.ttf) format('truetype')}";
+    style.innerHTML = openSans + " " + openSansBold;
+
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+  }
 
   function init() {
     view.attachListeners();
+
+    // checks for CSS Font Loading API avaiability
+    if ( document.fonts ) {
+      loadFonts();
+    } else {
+      loadFontsFallback();
+    }
   }
 
   var appAPI = {
@@ -96,13 +136,3 @@ var app = (function() {
 })();
 
 app.init();
-
-// TODO: make it load the font correctly
-// right now it says the font loaded if though it doesn't exist
-document.fonts.load("50px NotWorkingCorrectly")
-              .then(function() {
-                console.log("font loaded");
-              })
-              .catch(function() {
-                console.log("font failed to load")
-              });
